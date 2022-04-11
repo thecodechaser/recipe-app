3fdb3714f122df06e737d69dd86468d9d64142ce
Buyaki <rittahbuyaki@gmail.com>
2022-04-11 16:27:15 +0300

    Create a table for foods

---
+++ db/migrate/20220411132003_create_foods.rb
@@ -0,0 +1,11 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.references :user, null: false, foreign_key: {to_table: 'users'}, index: true
+      t.string :name
+      t.integer :measurement_unit
+      t.decimal :price
+      t.timestamps
+    end
+  end
+end
