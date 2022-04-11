1041c681112ab6ffa40a8540bab51a6b9dcccb91
Buyaki <rittahbuyaki@gmail.com>
2022-04-11 17:42:08 +0300

    Add new and create actions for the foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,22 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @new_food = current_user.foods.new(food_params)
+    if @new_food.save
+      redirect_to foods_path, flash: { alert: 'Your food is saved' } 
+    else
+      redirect_to new_food_path, flash: { alert: 'Could not save your food' }
+    end
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :foods
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :foods
+  
   validates :name, presence: true
 end
