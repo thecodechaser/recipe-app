41fedff16b7dc307fb50551460e38bc91502a392
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-12 14:05:17 +0530

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,33 +1,31 @@
+# frozen_string_literal: true
+
 class RecipesController < ApplicationController
-
-def index
-  @recipes = Recipe.all
-end
-
-def show
-
-end
-
-def create
-  @new_recipe = current_user.recipes.new(recipe_params)
-  if @new_recipe.save!
-    redirect_to recipes_path, flash: { alert: 'Your recipe is saved' }
-  else 
-    redirect_to new_recipe_path, flash: { alert: 'Could not save your recipe' }
+  def index
+    @recipes = Recipe.all
   end
-end
 
-def destroy
-  @recipe = Recipe.find(params[:id])
-  @recipe.destroy!
-  flash[:notice] = 'You have deleted the food!'
+  def show; end
+
+  def create
+    @new_recipe = current_user.recipes.new(recipe_params)
+    if @new_recipe.save!
+      redirect_to recipes_path, flash: { alert: 'Your recipe is saved' }
+    else
+      redirect_to new_recipe_path, flash: { alert: 'Could not save your recipe' }
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy!
+    flash[:notice] = 'You have deleted the food!'
     redirect_to recipes_path
-end
+  end
 
-private
+  private
 
-def recipe_params
-  params.require(:recipe).permit(:name, :prepration_time, :cooking_time, :description)
-end
-
+  def recipe_params
+    params.require(:recipe).permit(:name, :prepration_time, :cooking_time, :description)
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 class Recipe < ApplicationRecord
   belongs_to :user
-
 end
