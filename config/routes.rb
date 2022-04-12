743b9677536b5dd87fdb76b8decf8ca3f18fda78
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-12 14:38:50 +0530

    Add route for recipe show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :foods, only: [:index, :new, :create, :destroy]
 
-  resources :recipes, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
