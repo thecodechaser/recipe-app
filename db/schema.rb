960263ba5b7aabc40a3fb0bdce8e5895645cf114
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-12 19:13:45 +0530

    Add recipes_foods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_12_074110) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_12_133854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_074110) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
+  create_table "recipes_foods", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "food_id", null: false
+    t.integer "quantity", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_recipes_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipes_foods_on_recipe_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_12_074110) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "recipes", "users"
+  add_foreign_key "recipes_foods", "foods"
+  add_foreign_key "recipes_foods", "recipes"
 end
