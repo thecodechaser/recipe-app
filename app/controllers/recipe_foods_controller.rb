cf2e6c203f91d2b2374e2b9ec8a6c589211b15a0
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-13 00:09:56 +0530

    Add foods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,6 +1,4 @@
 class RecipeFoodsController < ApplicationController
-  # before_action :set_recipe_food, only: %i[show edit update destroy]
-  
   def new
     @recipe_food = RecipeFood.new
   end
@@ -8,7 +6,6 @@ class RecipeFoodsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.create(recipe_food_params)
-    # @new_recipe_food = RecipeFood.new(quantity: params[:quantity], food_id: params[:food_id], recipe_id: @recipe.id)
     if @recipe_food.save
       flash[:notice] = 'Ingredient Added successfully.'
       redirect_to @recipe
@@ -16,11 +13,6 @@ class RecipeFoodsController < ApplicationController
       render :new
     end
   end
-
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_recipe_food
-  #   @recipe_food = RecipeFood.find(params[:id])
-  # end
 
   private
 
