412482fb00d8e41195a3513f508a1fec632d2ef3
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-12 13:15:43 +0530

    Add migration file for recipes

---
+++ db/migrate/20220412074110_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.references :user, null: false, foreign_key: { to_table: 'users'}, index: true 
+      t.string :name
+      t.string :prepration_time
+      t.string :cooking_time
+      t.text :description
+      t.boolen :public
+      t.timestamps
+    end
+  end
+end
