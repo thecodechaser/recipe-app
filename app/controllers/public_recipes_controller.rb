18089e7ad567fce35288fd51bfbe4210d9784140
Buyaki <rittahbuyaki@gmail.com>
2022-04-13 14:30:35 +0300

    Add to the index file for public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class PublicRecipesController < ApplicationController
   def index
-    @public_recipes = Recipe.where(public: true).order(created_at: :desc).map do |recipe|
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc).map do |public_recipe|
       {
-        id: recipe.id,
-        name: recipe.name,
-        user: recipe.user.name,
-        description: recipe.description,
-        food_items: recipe.recipe_foods.count,
-        total_price: recipe.foods.map(&:price).sum
+        id: public_recipe.id,
+        name: public_recipe.name,
+        user: public_recipe.name,
+        description: public_recipe.description,
+        food_items: public_recipe.recipe_foods.count,
+        total_price: public_recipe.foods.map(&:price).sum
       }
     end
   end
