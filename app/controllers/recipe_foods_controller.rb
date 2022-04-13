204771bc0c36fea5b2bfa2dfcc5f261785568cd5
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-13 14:18:26 +0530

    Add functionality to remove ingredient

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -8,11 +8,17 @@ class RecipeFoodsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = @recipe.recipe_foods.create(recipe_food_params)
     if @recipe_food.save
-      flash[:notice] = 'Ingredient Added successfully.'
-      redirect_to @recipe
+      redirect_to recipe_path(params[:recipe_id]), flash: { alert: 'Your food is saved' }
     else
-      render :new
+      redirect_to new_recipe_recipe_food_path, flash: { alert: 'Could not save your food' }
     end
+  end
+
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy!
+    flash[:notice] = 'You have deleted the recipe food!'
+    redirect_to recipe_path(params[:recipe_id])
   end
 
   private
