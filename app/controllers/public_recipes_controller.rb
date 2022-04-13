2112db2395f70763354b93975ecd6a1a310f2796
Buyaki <rittahbuyaki@gmail.com>
2022-04-13 13:57:47 +0300

    Add functionality to the index method in the public recipes controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,14 @@
 class PublicRecipesController < ApplicationController
-    def index
+  def index
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc).map do |recipe|
+      {
+        id: recipe.id,
+        name: recipe.name,
+        user: recipe.user.name,
+        description: recipe.description,
+        food_items: recipe.recipe_foods.count,
+        total_price: recipe.foods.map(&:price).sum
+      }
     end
+  end
 end
