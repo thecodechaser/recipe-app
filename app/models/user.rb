34a10d5918b9f547e2ceb0ca7f4ab20e95ee4392
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-14 14:32:11 +0530

    Add validations to recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { in: 1..50 }
 
   def admin?(requested_role)
     role == requested_role.to_s
