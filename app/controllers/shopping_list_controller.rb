aa7c9c3cc5cdb851b43147c69bb6fa3f6e596f4a
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-14 14:10:30 +0530

    Add markup for shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,7 +1,28 @@
 class ShoppingListController < ApplicationController
 
   def index
+    @food_amount = 0;
+
+    current_user.recipes.each do |recipe|
+     @food_amount += recipe.recipe_foods.count
+    end
+
+
+     @total_price = 0;
+
+     current_user.recipes.each do |recipe|
+      recipe.recipe_foods.each do |recipe_food|
+        @total_price +=recipe_food.food.price*recipe_food.quantity
+      end
+    end
+
+    @recipe_foods = []
+
+    current_user.recipes.each do |recipe|
+      @recipe_foods.push(recipe.recipe_foods)
+    end
+
 
   end
-  
+
 end
