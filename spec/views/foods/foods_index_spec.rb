3e15bf35cf47ae014436635dc57fba1e853cb8b8
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-14 19:59:31 +0530

    Add tests for food#index

---
+++ spec/views/foods/foods_index_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'recipes#index', type: :feature do
+  describe 'Recipe' do
+    before(:each) do
+      @user = User.create(name: 'Ranjeet',  email: 'amy@gmail.com', password: 'password')
+      visit new_user_session_path
+      fill_in 'Email', with: 'amy@gmail.com'
+      fill_in 'Password', with: 'password'
+      click_button 'Log in'
+
+      @food1 = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 5)
+      @food2 = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 5)
+      @food3 = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 5)
+      @food4 = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 5)
+
+      visit(foods_path)
+    end
+
+    it 'shows the food name' do
+      expect(page).to have_content('Food')
+    end
+    
+  end
+end
