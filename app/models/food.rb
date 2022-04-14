c729ec529ddb7e970045ae2d7c43e4c35b994b73
Ranjeet Singh <ranjeetsingh969098@gmail.com>
2022-04-14 14:36:43 +0530

    Add validations for food

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,8 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
+
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :measurement_unit, presence: true, length: { in: 1..20 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 1 }
 end
